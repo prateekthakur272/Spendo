a41d22822687f0a6ed5e2100358896a939857da6
Prateek Thakur <prateekthakur272@gmail.com>
2025-10-23 19:05:38 +0530

    implements clicks on category

--- app/src/main/java/dev/prateekthakur/spendo/domain/models/CategoryExpense.kt
+++ app/src/main/java/dev/prateekthakur/spendo/domain/models/CategoryExpense.kt
@@ -1,6 +1,15 @@
 package dev.prateekthakur.spendo.domain.models
 
+import kotlin.collections.component1
+import kotlin.collections.component2
+
 data class CategoryExpense(
     val type: ExpenseType,
     val total: Double
 )
+
+fun List<Expense>.categoryExpenses() : List<CategoryExpense> {
+    return this.groupBy { it.type }.map { (k, v) ->
+        CategoryExpense(type = k, total = v.map { it.amount }.reduceOrNull { a, b -> a + b } ?: 0.0)
+    }
+}
